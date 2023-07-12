e075039ba3bbc700d7a59145f88762934ad9c19a
sudhanshu <sudhanshu.infograins@gmail.com>
2023-07-12 12:57:06 +0530

    fix: header and deposit bug

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -74,51 +74,52 @@ const HeaderNew = () => {
                                 navbarScroll
                             >
 
-                                {/* <Link to="/">Bridge</Link>
-                                <Link to="/account">Account</Link> */}
                             </Nav>
-                            <div className='header_btn_wrap'>
-                                {
-                                    isConnected ? <button className='btn disconnect_btn header_btn me-2'>{getNetwork}</button> : ""
-                                }
+                            <div className='right_header_wrap'>
 
-                                {/* {
+                                <div className='header_btn_wrap'>
+                                    {
+                                        isConnected ? <button className='btn disconnect_btn header_btn me-2'>{getNetwork}</button> : ""
+                                    }
+
+                                    {/* {
                                     getNetwork !== "Unsupported Network" ? <button className='btn disconnect_btn header_btn me-2'><FaEthereum /> {getNetwork}</button> : <button className='btn disconnect_btn header_btn me-2'><BiInfoCircle /> {getNetwork} </button>
                                 } */}
-                            </div>
-                            {/* <div className='header_btn_wrap'>
+                                </div>
+                                {/* <div className='header_btn_wrap'>
 
-                                {address ? <button className='btn disconnect_btn header_btn' onClick={() => handleDisconnect()}>
+{address ? <button className='btn disconnect_btn header_btn' onClick={() => handleDisconnect()}>
                                     <img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAYAAABzenr0AAAAsElEQVRYR2PslBX+z4AHHLiVgiLroDYHn3IGUtUzjjpg0IUAehyiRzipaYCQfow0MOoAuoeA5/dylHIAPY7RHWSt9Q5vOXD0mhDecgPdPMZRBwx4CJBaEOFNAFgkCZUbJNcFow4YDYHREBjwEKC0LkD3AMnlwKgDqB4CLYqpKO0BQvX5b5YgvOmQ9c86FHlC7QnGUQcMeAigN0jQIxg90aGnEUrVY7QJKTWQVAePOgAAXAoAZIiZ6M4AAAAASUVORK5CYII=" alt="Profile Icon" />
                                     {address.slice(0, 5)}...{address.slice(-5)}</button> : <button onClick={() => connect()} className='btn disconnect_btn header_btn'>Connect Wallet</button>}
                             </div> */}
 
-                            <div className='dropdown_wrap'>
-                                {checkMetaMask === true ? <a className='btn disconnect_btn header_btn' href='https://metamask.io/' target='_blank'><Image src={metamask} alt="metamask icn" fluid /> Please Install Metamask Wallet</a> : address ? <Dropdown>
-                                    <Dropdown.Toggle variant="success" id="race_header_dropdown" >
-                                        {address.slice(0, 5)}...{address.slice(-5)}
-                                    </Dropdown.Toggle>
-                                    <Dropdown.Menu>
-                                        <div className='user_profile_wrap'>
-                                            <figure className='user_profile'>
-                                                <Image src='data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAYAAABzenr0AAAAsElEQVRYR2PslBX+z4AHHLiVgiLroDYHn3IGUtUzjjpg0IUAehyiRzipaYCQfow0MOoAuoeA5/dylHIAPY7RHWSt9Q5vOXD0mhDecgPdPMZRBwx4CJBaEOFNAFgkCZUbJNcFow4YDYHREBjwEKC0LkD3AMnlwKgDqB4CLYqpKO0BQvX5b5YgvOmQ9c86FHlC7QnGUQcMeAigN0jQIxg90aGnEUrVY7QJKTWQVAePOgAAXAoAZIiZ6M4AAAAASUVORK5CYII=' alt="Profile Icon" />
-                                            </figure>
-                                            <h4>{address.slice(0, 5)}...{address.slice(-5)}
-                                                <OverlayTrigger
-                                                    placement="top"
-                                                    delay={{ show: 250, hide: 250 }}
-                                                    overlay={renderTooltip}>
-                                                    <CopyToClipboard text={address}>
-                                                        <span className="d-inline-block"><MdContentCopy onClick={handleSourceCopy} /> </span>
-                                                    </CopyToClipboard>
-                                                </OverlayTrigger>
-                                            </h4>
-                                        </div>
-                                        <Dropdown.Item as={Link} to="/account/deposit"><AiOutlineDownload /> View Deposit</Dropdown.Item>
-                                        <Dropdown.Item as={Link} to="/account/withdraw"><AiOutlineUpload /> View Withdrawals</Dropdown.Item>
-                                        <Dropdown.Item onClick={() => handleDisconnect()}><BiPowerOff /> Disconnect</Dropdown.Item>
-                                    </Dropdown.Menu>
-                                </Dropdown> : <button onClick={() => connect()} className='btn disconnect_btn header_btn'>Connect Wallet</button>}
+                                <div className='dropdown_wrap'>
+                                    {checkMetaMask === true ? <a className='btn disconnect_btn header_btn' href='https://metamask.io/' target='_blank'><Image src={metamask} alt="metamask icn" fluid /> Please Install Metamask Wallet</a> : address ? <Dropdown>
+                                        <Dropdown.Toggle variant="success" id="race_header_dropdown" >
+                                            {address.slice(0, 5)}...{address.slice(-5)}
+                                        </Dropdown.Toggle>
+                                        <Dropdown.Menu>
+                                            <div className='user_profile_wrap'>
+                                                <figure className='user_profile'>
+                                                    <Image src='data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAYAAABzenr0AAAAsElEQVRYR2PslBX+z4AHHLiVgiLroDYHn3IGUtUzjjpg0IUAehyiRzipaYCQfow0MOoAuoeA5/dylHIAPY7RHWSt9Q5vOXD0mhDecgPdPMZRBwx4CJBaEOFNAFgkCZUbJNcFow4YDYHREBjwEKC0LkD3AMnlwKgDqB4CLYqpKO0BQvX5b5YgvOmQ9c86FHlC7QnGUQcMeAigN0jQIxg90aGnEUrVY7QJKTWQVAePOgAAXAoAZIiZ6M4AAAAASUVORK5CYII=' alt="Profile Icon" />
+                                                </figure>
+                                                <h4>{address.slice(0, 5)}...{address.slice(-5)}
+                                                    <OverlayTrigger
+                                                        placement="top"
+                                                        delay={{ show: 250, hide: 250 }}
+                                                        overlay={renderTooltip}>
+                                                        <CopyToClipboard text={address}>
+                                                            <span className="d-inline-block"><MdContentCopy onClick={handleSourceCopy} /> </span>
+                                                        </CopyToClipboard>
+                                                    </OverlayTrigger>
+                                                </h4>
+                                            </div>
+                                            <Dropdown.Item as={Link} to="/account/deposit"><AiOutlineDownload /> View Deposit</Dropdown.Item>
+                                            <Dropdown.Item as={Link} to="/account/withdraw"><AiOutlineUpload /> View Withdrawals</Dropdown.Item>
+                                            <Dropdown.Item onClick={() => handleDisconnect()}><BiPowerOff /> Disconnect</Dropdown.Item>
+                                        </Dropdown.Menu>
+                                    </Dropdown> : <button onClick={() => connect()} className='btn disconnect_btn header_btn'>Connect Wallet</button>}
+                                </div>
                             </div>
                         </Navbar.Collapse>
                     </Container>
